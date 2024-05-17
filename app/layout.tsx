49cc10b7172ca475b74ca18ab75c380c3b81c068
Chaitanya-Chaurasia <98615500+Chaitanya-Chaurasia@users.noreply.github.com>
2024-05-18 00:53:40 +0530

    fixed layout.tsx for vercel compat.

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,6 @@ export default function RootLayout({
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
       <link
         rel="stylesheet"
-        precedence="default"
         href="https://fonts.googleapis.com/css2?family=Anonymous+Pro:ital,wght@0,400;0,700;1,400;1,700&display=swap"
       />
       <body className="min-h-screen flex flex-col bodymain">{children}</body>
