8a218e1b30c6cbc7aa17b9571a221d4bb963f308
Chaitanya-Chaurasia <cchauras@asu.edu>
2024-07-31 22:45:48 +0530

    Added draft work

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -9,12 +9,13 @@ import Work02 from "@/components/work/Work02";
 import Work01 from "@/components/work/Work01";
 import Work03 from "@/components/work/Work03";
 import Work04 from "@/components/work/Work04";
-
-import garfield from "@/public/garfield.gif";
-import comic from "@/public/comic.gif";
 import Free01 from "@/components/work/Free01";
 import Free02 from "@/components/work/Free02";
 import Free03 from "@/components/work/Free03";
+
+import garfield from "@/public/garfield.gif";
+import comic from "@/public/comic.gif";
+import { GitHubLogoIcon, InstagramLogoIcon, LinkedInLogoIcon, TwitterLogoIcon } from "@radix-ui/react-icons";
 
 const WorkPage = () => {
   return (
@@ -54,6 +55,73 @@ const WorkPage = () => {
           <Free01 />
           <Free02 />
           <Free03 />
+        </div>
+        <Separator className="w-96 my-10" />
+        <div className="flex flex-col items-center justify-center gap-2">
+          <h2 className="tracking-tight text-xl font-semibold">
+            As <span>Robert Frost</span>, so eloquently, writes,
+          </h2>
+          <br />
+          <br />
+          <span className="text-center">
+            Whose woods these are I think I know.
+            <br />
+            His house is in the village though; <br />
+            He will not see me stopping here
+            <br /> To watch his woods fill up with snow.
+          </span>
+          <br />
+          <span className="text-center">
+            My little horse must think it queer <br />
+            To stop without a farmhouse near
+            <br />
+            Between the woods and frozen lake <br />
+            The darkest evening of the year.
+          </span>
+          <br />
+          <span className="text-center">
+            He gives his harness bells a shake <br />
+            To ask if there is some mistake.
+            <br />
+            The only other sound's the sweep <br />
+            Of easy wind and downy flake.
+          </span>
+          <br />
+          <span className="text-center">
+            The woods are lovely, dark and deep,
+            <br />
+            But I have promises to keep,
+            <br /> And{" "}
+            <span className="font-medium bg-fuchsia-300 text-white">
+              miles to go
+            </span>{" "}
+            before I sleep,
+            <br /> And miles to go{" "}
+            <span className="font-medium bg-emerald-500 text-white">
+              before I sleep.
+            </span>
+          </span>
+          
+        </div>
+        <Separator className="w-96" />
+        <div className="flex items-center justify-center gap-10 mb-20">
+          <GitHubLogoIcon height={20} width={20} className="hover:scale-125" />
+          <LinkedInLogoIcon
+            height={20}
+            width={20}
+            className="hover:scale-125"
+          />
+          <TwitterLogoIcon height={20} width={20} className="hover:scale-125" />
+          <InstagramLogoIcon
+            height={20}
+            width={20}
+            className="hover:scale-125"
+          />
+
+          <Separator orientation="vertical" className="h-[20px]" />
+          <span className="font-medium text-muted-foreground text-sm">
+            cchauras@asu.edu
+          </span>
         </div>
       </div>
     </main>
--- components/work/Free03.tsx
+++ components/work/Free03.tsx
@@ -5,12 +5,19 @@ const Free03 = () => {
   return (
     <Card className="w-[350px] border-none">
       <CardHeader>
-      <h2 className="tracking-tight text-3xl font-bold flex gap-2 items-center">
-          Photography
+        <h2 className="tracking-tight text-3xl font-bold flex gap-2 items-center">
+          Travel & Photography
         </h2>
         <br />
-        Two things I really enjoy apart from work, is to travel and capture moments I've lived. Its truly a priviledge to travel, explore what different parts of the world have for me. I've been fortunate to have lived and visited different parts of the world whilst growing, and I'm really grateful for the experiences and memories I have carried. 
-        At my home, we have a Canon 3000D, nothing fancy, but really comes handy. I'm still a rookie, which is why I keep learning and, on top of that, do it because I enjoy pursuing it. Find some of my clicks below.
+        <span>
+          Two more things I really enjoy are travelling and capture some of the
+          moments I've lived. Its truly a privilege to travel, explore what
+          different parts of the world have in store.
+          <br />
+          <br /> I have a Canon 3000D, nothing fancy, but I'm still a rookie, which is why I keep learning and, on top of that,
+          do it because I enjoy pursuing it. Find some of my clicks in the
+          <span className="font-semibold"> Blogs</span> section.
+        </span>
       </CardHeader>
     </Card>
   );
