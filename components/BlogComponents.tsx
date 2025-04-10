8d205b886c01f43374505a0f381ae6ad42103ab8
Chaitanya-Chaurasia <cchauras@asu.edu>
2025-04-09 17:14:50 -0700

    ft: removed blog page for prod

--- components/BlogComponents.tsx
+++ components/BlogComponents.tsx
@@ -1,11 +1,11 @@
 import Link from "next/link";
 import Image from "next/image";
 import { ExternalLink } from "lucide-react";
-import art1 from "../public/art1.jpg"
-import art2 from "../public/art2.jpg"
-import art3 from "../public/art3.jpg"
-import art4 from "../public/art4.jpg"
-import art5 from "../public/art5.jpg"
+import art1 from "../public/art1.jpg";
+import art2 from "../public/art2.jpg";
+import art3 from "../public/art3.jpg";
+import art4 from "../public/art4.jpg";
+import art5 from "../public/art5.jpg";
 
 export const BlogTitle = () => {
   return (
@@ -51,19 +51,23 @@ export const MediumPosts = () => {
       title: "React.js: Meta's Biggest Boon",
       link: "https://medium.com/@chaitanyalvis/react-js-metas-biggest-boon-a3d32d4e2b4b",
       date: "Jan 27, 2025",
-      image: art3,
+      image: art4,
     },
     {
       title: "Joie de Vivre: A Relentless Pursuit of Happiness",
       link: "https://medium.com/@chaitanyalvis/joie-de-vivre-a-relentless-pursuit-of-happiness-61e153ee5d4c",
       date: "Jan 6, 2025",
-      image: art4,
+      image: art5,
     },
     {
       title: "People and The Adroit Art of Equivocating",
       link: "https://medium.com/@chaitanyalvis/people-and-the-adroit-art-of-equivocating-865ca457988f",
       date: "Dec 17, 2024",
-      image: art5,
+      image: art3,
+    },
+    {
+      title: "more's in the brew....",
+      link: "",
     },
   ];
 
@@ -78,9 +82,7 @@ export const MediumPosts = () => {
         </p>
       </div>
 
-      {/* Bento Grid Layout */}
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4 auto-rows-auto">
-        {/* Featured Article (Larger) */}
         <Link
           href={posts[0].link}
           target="_blank"
@@ -107,7 +109,6 @@ export const MediumPosts = () => {
           </div>
         </Link>
 
-        {/* Regular Articles */}
         {posts.slice(1).map((post, index) => (
           <Link
             href={post.link}
@@ -116,13 +117,17 @@ export const MediumPosts = () => {
             rel="noopener noreferrer"
             className="group relative overflow-hidden rounded-xl h-64"
           >
-            <div className="absolute inset-0 bg-black/60 z-10 transition-opacity group-hover:bg-black/70" />
-            <Image
-              src={post.image || "/placeholder.svg"}
-              alt={post.title}
-              fill
-              className="object-cover transition-transform duration-500 group-hover:scale-105"
-            />
+            {post.date && (
+              <div className="absolute inset-0 bg-black/60 z-10 transition-opacity group-hover:bg-black/70" />
+            )}
+            {post.image && (
+              <Image
+                src={post.image || "/placeholder.svg"}
+                alt={post.title}
+                fill
+                className="object-cover transition-transform duration-500 group-hover:scale-105"
+              />
+            )}
             <div className="absolute bottom-0 left-0 right-0 p-4 z-20 text-white">
               <div className="flex justify-between items-start">
                 <div>
